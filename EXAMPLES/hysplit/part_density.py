b90c9167d1fa59fdac7a6aa74a695210b9e94a5d
demichie <demichie@gmail.com>
2017-03-06 16:33:26 +0100

    bug fixed for particle density in hysplit input file

--- EXAMPLES/hysplit/part_density.py
+++ EXAMPLES/hysplit/part_density.py
@@ -21,19 +21,22 @@ def calc_density(diam):
 
     for i in range(npart):
 
-        if ( diam[i] < diam1[i] ):
+        if ( diam[i] < diam1[i]*1000 ):
 
             density[i] = rho1[i]
 
-        elif ( diam[i] < diam2[i] ):
+        elif ( diam[i] < diam2[i]*1000 ):
 
-            diam1_phi = -log(diam1(i_part)*1000)/log(2.0)
-            diam2_phi = -log(diam2(i_part)*1000)/log(2.0)
+            diam1_phi = -np.log(diam1[i]*1000)/np.log(2.0)
+            diam2_phi = -np.log(diam2[i]*1000)/np.log(2.0)
 
             density[i] = rho1[i] + ( diam_phi[i] - diam1_phi ) / ( diam2_phi - diam1_phi ) * ( rho2[i] - rho1[i] )
        
         else:
 
            density[i] = rho2[i]
+
+	print 'diam',diam[i],diam1[i],diam2[i],diam_phi[i],density[i]
+
        
     return density
